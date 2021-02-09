553117f31cadf9b4b96fd5ca676e07013c4200d8
yani82 <yani.iman1@gmail.com>
2021-02-09 00:24:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main#main').remove(); 
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory" 
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.classroom = "victory";
+
